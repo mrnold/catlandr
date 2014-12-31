37125da038596c7aeae44538b512db659e3aa1a1
Matthew Arnold <5075485+mrnold@users.noreply.github.com>
2014-12-31 12:43:22 -0500

    Make cat sit in the direction it was last facing.

--- Source/bitmap.h
+++ Source/bitmap.h
@@ -5,11 +5,12 @@ extern unsigned char img_lander[];
 extern unsigned char img_downcrash[];
 extern unsigned char img_leftcrash[];
 extern unsigned char img_rightcrash[];
-extern unsigned char img_thrustright[][THRUST_STAGES];
 extern unsigned char img_thrustleft[][THRUST_STAGES];
 extern unsigned char img_thrustdown[][THRUST_STAGES];
+extern unsigned char img_thrustright[][THRUST_STAGES];
 
 extern unsigned char cat_batting[8][CAT_RUNSTAGES];
-extern unsigned char cat_sitting[8][CAT_RUNSTAGES];
 extern unsigned char cat_runleft[8][CAT_RUNSTAGES];
 extern unsigned char cat_runright[8][CAT_RUNSTAGES];
+extern unsigned char cat_sittingleft[8][CAT_RUNSTAGES];
+extern unsigned char cat_sittingright[8][CAT_RUNSTAGES];
