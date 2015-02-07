4ecd09be15609a8041ec117c3c391327c19529f3
mjarnold <mjarnold@localhost>
2015-02-07 13:07:47 -0500

    Make sure python and sdcc are available before building anything.

--- build.bat
+++ build.bat
@@ -1,11 +1,86 @@
-@mkdir Build > NUL 2>&1
-@sdasz80 -p -g -o Build\ti86_crt0.rel Source\ti86_crt0.s
+@rem Check on prerequisites, sdcc >3.4.0 and python
+@if not {%PYTHONDIR%} == {} (
+    @if exist %PYTHONDIR%\python.exe (
+        @goto :pythonfound
+    )
+)
+@rem Python dir not set or python.exe not found, look in PATH
+@for %%p in (python.exe) do @set PYTHONDIR="%%~dp$PATH:p"
+@if {%PYTHONDIR%} == {""} (
+    @echo Could not find Python on your PATH or in PYTHONDIR!
+    @echo Please set PYTHONDIR to the full python folder, or add this to PATH.
+    @echo Example: set "PATH=%%PATH%%;C:\Program Files (x86)\Python"
+    @echo Example: set PYTHONDIR="C:\Program Files (x86)\Python"
+    @exit /b
+)
+:pythonfound
+@echo Found python.exe at %PYTHONDIR%
 
+@rem Look for SDCC
+@if not {%SDCCDIR%} == {} (
+    @if exist %SDCCDIR%\sdcc.exe (
+        @goto :sdccfound
+    )
+)
+@rem SDCC dir not set or sdcc.exe not found, look in PATH
+@for %%s in (sdcc.exe) do @set SDCCDIR="%%~dp$PATH:s"
+@if {%SDCCDIR%} == {""} (
+    @echo Could not find SDCC on your PATH or in SDCCDIR!
+    @echo Please set SDCCDIR to the full path to SDCC\bin, or add this to PATH.
+    @echo Example: set "PATH=%%PATH%%;C:\Program Files (x86)\SDCC\bin"
+    @echo Example: set SDCCDIR="C:\Program Files (x86)\SDCC\bin"
+    @exit /b
+)
+:sdccfound
+
+@rem Parse the output of "sdcc --version" and look for an X.Y.Z version string
+@for /f "usebackq tokens=1-10" %%a in (`call %SDCCDIR%\sdcc.exe -v`) do @(
+    @for %%m in (%%a %%b %%c %%d %%e %%f %%g %%h %%i %%j) do @(
+        @echo %%m | findstr /r "[0-9]*\.[0-9]*\.[0-9]" > NUL 2>&1
+        @if not ERRORLEVEL 1 (
+            @set SDCCVER=%%m
+            @goto :sdccversion
+        )
+    )
+)
+@echo Found sdcc.exe, but could not determine version!
+@echo This program depends on SDCC 3.4.0 or newer.
+@echo The sdcc.exe in question is in %SDCCDIR%
+@exit /b
+:sdccversion
+@echo Found sdcc.exe version %SDCCVER% in %SDCCDIR%
+@rem From version X.Y.Z, make sure X is at least 3.
+@rem If it is exactly 3, make sure Y is at least 4.
+@for /f "tokens=1-3 delims=." %%a in ("%SDCCVER%") do @(
+    @if %%a geq 3 (
+        @if %%a equ 3 (
+            @if %%b lss 4 (
+                @echo Detected SDCC minor version less than 4.
+                @goto :sdcctooold
+            )
+        )
+    ) else (
+        @echo Detected SDCC major version less than 3.
+        @goto :sdcctooold
+    )
+    @goto :sdccok
+)
+:sdcctooold
+@echo This program requires a minimum of SDCC 3.4.0.
+@exit /b
+:sdccok
+
+@set SDCC=%SDCCDIR%\sdcc.exe
+@set SDAS=%SDCCDIR%\sdasz80.exe
+@set PYTHON=%PYTHONDIR%\python.exe
 @set LINKS=Build\ti86_crt0.rel
 @set BUILD=@call :build
 @set COMPILE=@call :compile
 @set FAKE=@call :fake
-@set SDCCBASE=sdcc -mz80 --no-std-crt0 --reserve-regs-iy --opt-code-speed -ISource
+@set SDCCBASE=%SDCC% -mz80 --no-std-crt0 --reserve-regs-iy --opt-code-speed -ISource
+
+@mkdir Build > NUL 2>&1
+%SDAS% -p -g -o Build\ti86_crt0.rel Source\ti86_crt0.s
 
 %COMPILE% Source\bitmap.c
 %COMPILE% Source\camera.c
@@ -21,13 +96,13 @@
 %BUILD% Source\main.c
 
 @rem SDCC generates main.ihx instead of main.c.ihx?
-python Tools\ihxtobin.py Build\Source\main.ihx
+%PYTHON% Tools\ihxtobin.py Build\Source\main.ihx
 
 @rem Patch the binary to initialize globals. Usually SDCC expects this to be
 @rem done in the startup code, but this is not necessary on a TI.
-python Tools\trim.py Build\Source\main.c.map Build\Source\main.ihx.bin 0xD748
+%PYTHON% Tools\trim.py Build\Source\main.c.map Build\Source\main.ihx.bin 0xD748
 @copy /y Build\Source\main.ihx.bin Build\Source\catlandr.bin
-python Tools\binto86p.py Build\Source\catlandr.bin
+%PYTHON% Tools\binto86p.py Build\Source\catlandr.bin
 
 @copy /y Build\Source\catlandr.86p catlandr.86p
 @goto :done
