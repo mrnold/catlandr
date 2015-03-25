0bd15b9589f60336840122a070e4e04148c2800d
Matthew Arnold <5075485+mrnold@users.noreply.github.com>
2015-03-24 21:49:28 -0400

    Functions with local variables should not be marked naked.
    Fixes immediate crash on TI86, but pushes slightly over the size limit.

--- Source/calc/ti86/display.h
+++ Source/calc/ti86/display.h
@@ -1,6 +1,6 @@
 #define SCREEN_WIDTH 128
 #define SCREEN_HEIGHT 64
 
-void prerender(void) __naked;
+void prerender(void);
 void save_graphbuffer(void) __naked;
 void restore_graphbuffer(void) __naked;
